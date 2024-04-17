2ca172f2326c0860c045380810d8d38acc8386e9
labinfo1 <labinfo1@ectufrn.ect.ufrn.br>
2024-04-17 10:10:59 -0300

    botao reset

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import { useState } from 'react';
 
 export default function Board() {
+  const [xIsNext, setXIsNext] = useState(true);
   const [squares, setSquares] = useState(Array(9).fill(null));
 
   function handleClick(i) {
+    if (squares[i] || calculateWinner(squares)) {
+      return;
+    }
     const nextSquares = squares.slice();
-    nextSquares[i] = "X";
+    if (xIsNext) {
+      nextSquares[i] = "X";
+    } else {
+      nextSquares[i] = "O";
+    }
+
     setSquares(nextSquares);
+    setXIsNext(!xIsNext);
+  }
+
+  function reset() {
+    setSquares(Array(9).fill(null))
   }
 
   return (
@@ -26,10 +40,31 @@ export default function Board() {
       <Square value={squares[7]} onSquareClick={() => handleClick(7)}/>
       <Square value={squares[8]} onSquareClick={() => handleClick(8)}/>
     </div>
+    <button onClick={reset}>Resetar o jogo</button>
     </>
   )
 
   function Square({ value, onSquareClick }) {
     return <button className="square" onClick={onSquareClick}>{value}</button>
   }
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
